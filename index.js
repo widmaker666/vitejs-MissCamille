8586b59f0074f3342da9a18d19feab52551ffd12
widmaker666 <alexandre.hontcharouk69@gmail.com>
2023-05-03 18:39:52 +0200

    début du responsive header il faut le finir. y manque de responsive

--- index.js
+++ index.js
@@ -1021,3 +1021,42 @@ function sendMail() {
       alert("Ton message est bien envoyé ! merci");
     });
 }
+
+//* text anime //
+const target = document.getElementById("target");
+let array = ["simplement\ féminin...", "une\ idée,\ un\ rêve,\ un\ chemin...", "le\ détail\ est\ humain...", "une\ création\ au\ quotidien...", "le\ geste\ vient\ du\ destin..."];
+let wordIndex = 0;
+let letterIndex = 0;
+
+/* textContent permet injecter du texte dans la balise h3 en l'occurence */
+
+const createLetter = () => {
+  const letter = document.createElement("span");
+  target.appendChild(letter);
+  letter.textContent = array[wordIndex][letterIndex];
+
+  setTimeout(() => {
+    letter.remove();
+  }, 2800);
+};
+
+const loop = () => {
+  setTimeout(() => {
+    if (wordIndex >= array.length) {
+      wordIndex = 0;
+      letterIndex = 0;
+      loop();
+    }else if (letterIndex < array[wordIndex].length) {
+      createLetter();
+      letterIndex++;
+      loop();
+    } else {
+      wordIndex++;
+      letterIndex = 0;
+      setTimeout(() => {
+        loop();
+      }, 2800);
+    }
+  }, 60);
+};
+loop();
